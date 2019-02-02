9cd41bc3572d49796bff7b435b925b341b53a605
Gungun <gungun@Gunguns-MacBook-Pro.local>
2019-02-02 12:58:20 +0530

    first commit

---
+++ src/app/typescriptcode.ts
@@ -0,0 +1,164 @@
+// var myfunc = (x) => x + 1;
+
+// var x: number = myfunc(1)
+
+// alert(x)
+
+
+
+// // function helllo(x)
+// // {
+
+// //   return x+1
+// // }
+
+
+// var helllo = (x) => x + 1
+// var result = helllo(2)
+// alert(result)
+// declaration
+/*var a: number = 10;
+var b: number;
+var c;*/
+// var a = 1;
+
+// if (a < 10)
+// {
+
+//     console.log("  a is less then 10")
+// }
+// else {
+    
+//     console.log(" a is greater ")
+// }
+
+// var a:number=10
+// while (a < 10)
+// {
+
+//     a++
+//     console.log(a)
+// }
+
+
+// for (var i = 0; i < 10; i++)
+// {
+
+//     console.log("i==",i)
+// }
+// var d=0
+// do {
+    
+// d+=1
+// }while(d<15)
+
+//add 
+// function add (a:number,b:number):number
+// {
+
+
+
+// return a+b
+
+
+// }
+
+// var results = add(2, 3)
+// alert(results)
+
+// class Student
+// {   
+//     constructor(id:number,_name:string,phno:string,address:string)
+//     {
+
+//         console.log(id, name, phno, address)
+//         this.name=_name
+//     }
+
+//     id: number;
+//    private name: string;
+//     phone_no: string;
+//     address: string;
+//     public getName()
+//     {
+//     return this.name
+
+//     }
+//     public setName(_name:string)
+//     {
+//      this.name=_name
+//     }
+// }
+
+// let s1 = new Student(1, "abhishek", "56676766", "hinjewadi")
+
+// //s1.setName("ravi")
+// console.log(s1.getName)
+
+
+
+
+
+// class Emp
+// {
+
+//     public display()
+//     {
+
+//         console.log("emp name")
+//     }
+// }
+
+// class Dept extends Emp
+// {
+
+//     public departnent()
+//     {
+//         console.log("dept")
+//     }
+// }
+// let d1 = new Dept()
+// d1.
+// let a=1=
+
+// interface Iperson
+
+// {
+
+//     hello();
+
+// }
+
+// class Emp implements Iperson
+// {
+//     public hello() {
+
+//     alert('hello emp')
+//  }
+
+// } 
+
+// let e1 = new Emp()
+// e1.hello()
+
+
+// interface Isports
+// {
+
+//     play();
+
+// }
+
+
+// class CricketTeam extends A implements Isports
+// {
+
+//     play()
+//     {
+    
+//     alert('i am playing cricket')
+// }
+
+// }
+// let c1 = new CricketTeam()
+// c1.play()
